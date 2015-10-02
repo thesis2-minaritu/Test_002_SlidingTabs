f1fa9598a82f91dc95d29099080fa95182da9a65
ciemira <chameleon.armie@gmail.com>
2015-10-02 16:32:34 -0700

    SLiding tabs_2nd Test_AC
    Build displays no error. With FUnctional frags and .xml for each tabs.
    For testing

---
+++ Test_002/app/src/main/java/com/minaritu/mobapp/test_002/ViewPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.minaritu.mobapp.test_002;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
+        super(fm);
+
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+
+    }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position == 0) // if the position is 0 we are returning the First tab
+        {
+            tab_home tabHome = new tab_home();
+            return tabHome;
+        }
+        if(position == 1) // if the position is 1 we are returning the Second tab
+        {
+            tab_logs tabLogs = new tab_logs();
+            return tabLogs;
+        }
+        else
+        {
+            tab_about tabAbout = new tab_about();
+            return tabAbout;
+        }
+
+
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    // This method return the Number of tabs for the tabs Strip
+
+    @Override
+    public int getCount() {
+        return NumbOfTabs;
+    }
+}
